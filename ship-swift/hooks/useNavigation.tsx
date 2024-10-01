4ce5413bf80a6de14b7ab9b6acfb6a7b21f6ac3c
Kamohelo Koali <kamohelokoali201@gmail.com>
2024-10-01 16:55:08 +0200

    navbar for chat

--- ship-swift/hooks/useNavigation.tsx
+++ ship-swift/hooks/useNavigation.tsx
@@ -1,16 +1,26 @@
-import { MessageCircle } from "lucide-react"
-import { usePathname } from "next/navigation"
-import { useMemo } from "react"
+import { HouseIcon, MessageCircle } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 export const useNavigation = () => {
-    const pathname = usePathname()
+  const pathname = usePathname();
 
-    const paths = useMemo(() => [
-        {
-            name: "Conversations",
-            href: "/conversations",
-            icon: <MessageCircle />,
-            active: pathname.startsWith("/conversations")
-        }
-    ], [pathname])
-}
+  const paths = useMemo(
+    () => [
+      {
+        name: "Home",
+        href: "/",
+        icon: <HouseIcon />,
+      },
+      {
+        name: "Conversations",
+        href: "/conversations",
+        icon: <MessageCircle />,
+        active: pathname.startsWith("/conversations"),
+      },
+    ],
+    [pathname]
+  );
+
+  return paths;
+};
