a56bec45a35c224c628ea15f5d4175f8a63f61e1
Matross-20 <matrossmthawelanga@gmail.com>
2024-10-02 12:06:19 +0200

    added somethings by kamohelo

---
+++ ship-swift/app/onboarding/driver-onboarding/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <Button>Driver</Button>
+  )
+}
+
+export default Page
