e202f377932243be3a33f04393885d82bbd45859
Ntlele <ntleledavid07@gmail.com>
2024-10-04 15:07:19 +0200

    edited my jobs page

--- ship-swift/screens/courier/dashboard/components/myjobs.tsx
+++ ship-swift/screens/courier/dashboard/components/myjobs.tsx
@@ -87,19 +87,19 @@ export default function MyJobs() {
             <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
               <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
                 <CardHeader className="pb-3">
-                  <CardTitle>Your Deliveries</CardTitle>
+                  <CardTitle>My Deliveries</CardTitle>
                   <CardDescription className="max-w-lg text-balance leading-relaxed">
-                    Get the status of your deliveries
+                    Get feedback of my progress
                   </CardDescription>
                 </CardHeader>
                 <CardFooter>
-                  <Button>Check status</Button>
+                  <Button>Get feedback</Button>
                 </CardFooter>
               </Card>
               <Card x-chunk="dashboard-05-chunk-1">
                 <CardHeader className="pb-2">
                   <CardDescription>This Week</CardDescription>
-                  <CardTitle className="text-4xl">$1,329</CardTitle>
+                  <CardTitle className="text-4xl">M 0.00</CardTitle>
                 </CardHeader>
                 <CardContent>
                   <div className="text-xs text-muted-foreground">
@@ -113,7 +113,7 @@ export default function MyJobs() {
               <Card x-chunk="dashboard-05-chunk-2">
                 <CardHeader className="pb-2">
                   <CardDescription>This Month</CardDescription>
-                  <CardTitle className="text-4xl">$5,329</CardTitle>
+                  <CardTitle className="text-4xl">M 0.00</CardTitle>
                 </CardHeader>
                 <CardContent>
                   <div className="text-xs text-muted-foreground">
