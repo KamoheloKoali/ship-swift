7e71b8414c5d7a1f6ee5b7d8625f38251403e6b3
khiba-k <khibakoenane@gmail.com>
2024-10-04 10:23:56 +0200

    driver dashboard

---
+++ ship-swift/app/(driver)/driver/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import UserProfile from "@/screens/courier/dashboard/components/UserProfile";
+import React from "react";
+// import { currentUser } from "@clerk/nextjs/server";
+
+export default async function Page() {
+  // const user = await currentUser();
+  return (
+    <div className="flex flex-row justify-center lg:justify-start">
+      {/* Driver: {user?.fullName} */}
+      <div className="hidden lg:w-[16%] lg:block">First</div>
+
+      <div className="flex flex-row w-full lg:w-[68%] justify-center">
+        <div className="w-[80%] lg:w-[72%]">
+          <UserProfile />
+        </div>
+        <div className="hidden lg:block w-[28%]">Child2</div>
+      </div>
+
+      <div className="hidden lg:w-[16%] lg:block">Third</div>
+    </div>
+  );
+}
