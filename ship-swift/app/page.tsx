c6a3c4a246dd8274a2459a5d63f51e440a08cfa7
Ntlele <ntleledavid07@gmail.com>
2024-10-09 22:36:15 +0200

    just created client again

--- ship-swift/app/page.tsx
+++ ship-swift/app/page.tsx
@@ -4,42 +4,42 @@ import getCurrentUserClerkDetails from "@/app/utils/getCurrentUserDetails";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // const client = await getCurrentUserClerkDetails();
-  // const dbClient = await getClientById(client?.id || "");
+  const client = await getCurrentUserClerkDetails();
+  const dbClient = await getClientById(client?.id || "");
 
-  // if (!dbClient.success) {
-  //   const clientData = {
-  //     clerkId: client?.id || "",
-  //     email: client?.emailAddresses[0]?.emailAddress || "",
-  //     phoneNumber: client?.phoneNumbers?.[0]?.phoneNumber || "", // Assuming phoneNumbers is an array
-  //     firstName: client?.firstName || "",
-  //     lastName: client?.lastName || "",
-  //     photoUrl: client?.imageUrl || "",
-  //     idPhotoUrl: "", // Assuming you will fetch or add the actual value later
-  //   };
-
-  //   await createClient(clientData);
-  // }
-
-  const Driver = await getCurrentUserClerkDetails()
-    const dbDriver = await getDriverById(Driver?.id || "")
-
-    if (!dbDriver.success) {
-
-    const DriverData = {
-      clerkId: Driver?.id || "",
-      email: Driver?.emailAddresses[0].emailAddress || "",
-      phoneNumber: Driver?.phoneNumbers?.[0]?.phoneNumber || "", // Assuming phoneNumbers is an array
-      firstName: Driver?.firstName || "",
-      lastName: Driver?.lastName || "",
-      photoUrl: Driver?.imageUrl || "",
+  if (!dbClient.success) {
+    const clientData = {
+      clerkId: client?.id || "",
+      email: client?.emailAddresses[0]?.emailAddress || "",
+      phoneNumber: client?.phoneNumbers?.[0]?.phoneNumber || "", // Assuming phoneNumbers is an array
+      firstName: client?.firstName || "",
+      lastName: client?.lastName || "",
+      photoUrl: client?.imageUrl || "",
       idPhotoUrl: "", // Assuming you will fetch or add the actual value later
-      vehicleType: "",
-      vehicleDetails: [""],
-    }
+    };
 
-    await createDriver(DriverData)
+    await createClient(clientData);
   }
+
+  // const Driver = await getCurrentUserClerkDetails()
+  //   const dbDriver = await getDriverById(Driver?.id || "")
+
+  //   if (!dbDriver.success) {
+
+  //   const DriverData = {
+  //     clerkId: Driver?.id || "",
+  //     email: Driver?.emailAddresses[0].emailAddress || "",
+  //     phoneNumber: Driver?.phoneNumbers?.[0]?.phoneNumber || "", // Assuming phoneNumbers is an array
+  //     firstName: Driver?.firstName || "",
+  //     lastName: Driver?.lastName || "",
+  //     photoUrl: Driver?.imageUrl || "",
+  //     idPhotoUrl: "", // Assuming you will fetch or add the actual value later
+  //     vehicleType: "",
+  //     vehicleDetails: [""],
+  //   }
+
+  //   await createDriver(DriverData)
+  // }
 
   return redirect("/conversations");
 }
