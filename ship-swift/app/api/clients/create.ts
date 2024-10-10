660111385ada1d20a7ae97e7068ae7e928cbf002
Matross-20 <matrossmthawelanga@gmail.com>
2024-10-10 18:14:54 +0200

    client

---
+++ ship-swift/app/api/clients/create.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/app/utils/db';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { name, email, phone, address } = req.body;
+
+    try {
+      const newClient = await prisma.clients.create({
+        data: { firstName, email, phone, address },
+      });
+      res.status(201).json(newClient);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to create client' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
