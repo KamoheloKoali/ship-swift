a13459b59b31a4f95afccd08589be092d797531d
Ntlele <ntleledavid07@gmail.com>
2024-11-02 02:04:38 +0200

    Take picture Done

--- ship-swift/app/onboarding/driver-onboarding/face-recog/page.tsx
+++ ship-swift/app/onboarding/driver-onboarding/face-recog/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import FaceMeshVerification from "@/screens/courier/dashboard/components/FaceMeshVerification";
+import PhotoCapture from "@/screens/courier/dashboard/components/FaceMeshVerification";
 
-
-export default function Face(){
-    return(
-        <>
-        <FaceMeshVerification/>
-        </>
-    )
+export default function Face() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-full border sm:p-2 xl:p-10">
+        <PhotoCapture />
+      </div>
+    </>
+  );
 }
